6afc46226abf8484918f938768db0b431842182d
juani981 <juanicarrizo97@gmail.com>
2022-07-07 01:20:58 -0300

    .gitignore actualizado para no mostrar el programa de debug.

--- JuegoRPG/Combate.cs
+++ JuegoRPG/Combate.cs
@@ -2,22 +2,23 @@
 //Clase que definirá los valores dentro de un combate
 public class Combate
 {
-	public Combate()
+	public Combate(Character personaje)
 	{
 		Random random = new Random();
-		Character personaje = new Character();
+		//Character personaje = new Character();
 		PD = PoderdeDisparo(personaje);//Poder de disparo
-		ED = random.Next(1, 101); //Efectividad de disparo.Tomar como valor porcentual
-		VA = PD * ED;//Valor de Ataque
+		ED = random.Next(1, 101); //Efectividad de disparo porcentual
+		VA = PD * ED/100;//Valor de Ataque
 		PDEF = PoderdeDefensa(personaje);//Poder de defensa
 		MDP = 50000;//Maximo danio provocable
+		Danio =DanioProvocado();
 	}
 	public int PD { get; set; }
 	public int ED { get; set; }
-	public int VA { get; set; }
+	public double VA { get; set; }
 	public int PDEF { get; set; }
 	public int MDP { get; set; }
-
+	public double Danio { get; set; }
 	public int PoderdeDisparo(Character personaje)
     {
 		int podDisparo = personaje.destreza * personaje.fuerza * personaje.nivel;
@@ -28,12 +29,14 @@ public class Combate
 		int podDefensa = personaje.armadura * personaje.velocidad;
 		return podDefensa;
     }
-	public int ActualizarSalud(Caracteristicas personaje)
+	public double DanioProvocado()
     {
-		int danioProvocado = ((VA * ED) - PDEF) / MDP;
-		//danio se calcula sin factor de correccion *100, ya que el valor porcentual de ED es int
-		return danioProvocado;
-		//personaje.salud = personaje.salud - danioProvocado;
-    }
-
+		double danioProvocado = Math.Truncate((((VA * ED) - PDEF) / MDP)*100);
+        if (danioProvocado>MDP)
+        {
+			return MDP;
+        }
+		else
+			return danioProvocado;
+	}
 }
--- JuegoRPG/Programa de Pruebas.cs
+++ JuegoRPG/Programa de Pruebas.cs
@@ -16,7 +16,7 @@ for (int i = 0; i < n; i++)
     //Se crea un elemnto de cada lista y se agrega a su respectiva lista
     Character personaje = new Character();
     Caracteristicas caracteristicasDelPersonaje = new Caracteristicas();
-    Combate atributosdepeleaDelPersonaje =new Combate();
+    Combate atributosdepeleaDelPersonaje =new Combate(personaje);
     characters.Add(personaje);
     caracteristicas.Add(caracteristicasDelPersonaje);
     combat.Add(atributosdepeleaDelPersonaje);
@@ -42,6 +42,114 @@ for (int i = 0; i < characters.Count(); i++)
     Console.WriteLine("Valor de Ataque: " + combat[i].VA);
     Console.WriteLine("Poder de Defensa: " + combat[i].PDEF);
     Console.WriteLine("Maximo danio provocable: " + combat[i].MDP);
-    Console.WriteLine("Danio provocado: "+ combat[i].ActualizarSalud(caracteristicas[i]));
+    Console.WriteLine("Danio provocado: " + combat[i].Danio);
     Console.WriteLine("=========");
+}
+
+//Programa de prueba de combate
+    //Defino dos indices aleatorios, para pelear entre si
+        //Se debe controlar que ambos valores no sean iguales, y no se repitan luego
+Random rnd = new Random();
+int peleador1, peleador2;
+List<int> indices = new List<int>();//Voy a llevar registro de los indices mediante una lista
+          //Generador de 2 indices aleatorios
+          //El par de indices debe generarse n/2 veces
+for (int i = 0; i < n/2; i++)
+{
+    do
+    {
+        peleador1 = rnd.Next(0, n);
+        peleador2 = rnd.Next(0, n); ;//n estaba definida antes como la cantidad de personajes a generar
+    } while (peleador1 == peleador2 || indices.Contains(peleador1) || indices.Contains(peleador2));
+    //Si ambos pasaron el control, los agrego a la Lista de indices
+    indices.Add(peleador1);
+    indices.Add(peleador2);
+}
+
+//Muestra de los pares de indices a pelear
+int k = 0;
+while(k<n)
+{
+    Console.WriteLine("Proxima batalla: " + indices[k] + " contra " + indices[k+1]);
+    k+=2;
+}
+
+//Los contrincantes estan definidos
+//Ahora se debe iniciar las batallas
+//3 ataques deben definirse para cada uno
+
+//characters[]      caracteristicas[]      combat[]
+
+/*Voy a usar como parametro principal para modificar el ataque, el nivel del personaje.
+ A menor nivel, mas favorecido sera el ataque de este personaje*/
+//Se van a modificar sus valores de combate de forma temporal durante su turno de 3 ataques.
+//Guardo los valores originales en variables auxiliares lo que dure el turno
+        //int index=0;
+        //int j = indices[0];
+for (int index=0; index < 2; index++)
+{
+    int j=indices[index];
+    int EfectividadDisparoAux = combat[j].ED;
+    double ValorDeAtaqueAux = combat[j].VA;
+    double danioprovocadoAux = combat[j].Danio;
+
+        //Ataque 1: 'Precision maxima' 
+        if (characters[j].nivel <= 3)
+        {
+            combat[j].ED += 20;//A bajo nivel, bonus de 20% de precision al primer ataque
+        }
+        else if (characters[j].nivel <= 6)
+        {
+            combat[j].ED += 8;//A medio nivel, bonus de 8% de precision al primer ataque
+        }                     //A alto nivel, ningun bonus de precision al primer ataque
+        Console.WriteLine("Ataque 1:--");
+        Console.WriteLine("Efectividad de Disparo: " + combat[j].ED);
+        Console.WriteLine("Valor de Ataque: " + combat[j].VA);
+        Console.WriteLine("Danio provocado: " + combat[j].DanioProvocado());
+
+        combat[j].ED = EfectividadDisparoAux;//Devuelvo a su valor original
+        //Ataque 2: 'Buff en el Valor de Ataque'
+        if (characters[j].nivel <= 3)
+        {
+            combat[j].VA += combat[j].VA * 0.05;//5% bonus en Valor de Ataque Si es Nivel Bajo
+        }
+        else if (characters[j].nivel <= 6)
+        {
+            combat[j].VA += combat[j].VA * 0.02;//2% bonus en Valor de ataque Si es Nivel Medio
+        }
+        Console.WriteLine("Ataque 2:--");
+        Console.WriteLine("Efectividad de Disparo: " + combat[j].ED);
+        Console.WriteLine("Valor de Ataque: " + combat[j].VA);
+        Console.WriteLine("Danio provocado: " + combat[j].DanioProvocado());
+        //Devuelvo a su valor original
+        combat[j].VA = ValorDeAtaqueAux;
+    //Ataque 3: 'Golpe Critico' : Si la diferencia de Salud es mayor a 50%, se hace el doble de danio, independiente del nimvel
+    if (Math.Abs(caracteristicas[j].salud - caracteristicas[indices[index+1]].salud) > 50)
+    {
+        if (caracteristicas[j].salud < caracteristicas[indices[index+1]].salud)
+        {
+            combat[j].Danio *= 2;
+            Console.Write("\tGOLPE CRITICO!\n");
+        }
+        /*else
+            combat[indices[index+1]].Danio *= 2;*/
+    }
+    Console.WriteLine("Ataque 3--");
+    Console.WriteLine("Efectividad de Disparo: " + combat[j].ED);
+    Console.WriteLine("Valor de Ataque: " + combat[j].VA);
+    Console.WriteLine("Danio provocado: " + combat[j].DanioProvocado());
+
+    combat[j].Danio = danioprovocadoAux;//Devuelvo a su valor original
+
+    //Muestra si se hicieron los cambios, solo DEBUG
+    Console.WriteLine("Primera batalla:");
+    Console.WriteLine("Atributos aleatorios del personaje " + indices[index]);
+    Console.WriteLine("Nivel:" + characters[j].nivel);
+    Console.WriteLine("Salud(100):" + caracteristicas[j].salud);
+    Console.WriteLine("Tipo:" + caracteristicas[j].tipo);
+    Console.WriteLine("Atributos de combate del personaje:");
+    Console.WriteLine("Poder de Disparo: " + combat[j].PD);
+    Console.WriteLine("Efectividad de Disparo: " + combat[j].ED);
+    Console.WriteLine("Valor de Ataque: " + combat[j].VA);
+    Console.WriteLine("Danio provocado: " + combat[j].DanioProvocado());
 }
